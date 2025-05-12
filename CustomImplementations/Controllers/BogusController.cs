254bc4b0e8803cefb43ad3d58a3cff408a2becaa
Lazyprogrammerrr <aakashsharma344@gmail.com>
2025-05-12 12:43:27 +0530

    Added New Bogus Controller for testing purpose

---
+++ CustomImplementations/Controllers/BogusController.cs
@@ -0,0 +1,17 @@
+﻿using CustomImplementations.CustomCache;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomImplementations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BogusController : ControllerBase
+    {
+        private readonly ICacheService _cacheService;
+        public BogusController(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+    }
+}
---
+++ CustomImplementations/Data/FakeUserGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using CustomImplementations.Models;
+
+namespace CustomImplementations.Data
+{
+    public static class FakeUserGenerator
+    {
+        public static FakeUser Generate()
+        {
+            var faker = new Faker<FakeUser>()
+                .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+                .RuleFor(u => u.LastName, f => f.Name.LastName())
+                .RuleFor(u => u.Email, f => f.Internet.Email())
+                .RuleFor(u => u.Phone, f => f.Phone.PhoneNumber());
+
+            return faker.Generate();
+        }
+    }
+}
